19fa4b7220231d04e760732f05c14aba6c9a6496
Opilane TTHK <opilane@tthk.ee>
2021-10-19 11:25:19 +0300

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number:");
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number");
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vali tehe ('+', '-', '*' või '/')");
+            char useroperation = Convert.ToChar(Console.ReadLine());
+
+            switch (useroperation)
+            {
+                case '+':
+                    AddTwoNumbers(firstnumber, secondnumber);
+                    break;
+                case '/':
+                    DivideTwoNumber(firstnumber, secondnumber);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(firstnumber, secondnumber);
+                    break;
+                case '-':
+                    SubtractTwoNumbers(firstnumber, secondnumber);
+                    break;
+                default:
+                    Console.WriteLine("vale tehe");
+                    break;
+                
+            }
+
+
+            
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+        private static void DivideTwoNumber(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+        }
+        private static void MultiplyTwoNumbers(int x, int y)
+        {
+            int result = x * y;
+            Console.WriteLine($"{x} * {y} = {result}");
+        }
+        private static void SubtractTwoNumbers(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine($"{x} - {y} = {result}");
+        }
+
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma nimi:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Sisesta oma vanus:");
+            int userage = Convert.ToInt32(Console.ReadLine());
+
+            sayhello(username, userage);
+        }
+
+        private static void sayhello(string somename, int somenumber)
+        {
+            Console.WriteLine($"Hi there! {somename}");
+            Console.WriteLine($"Oled sündinud aastal {2021 - somenumber}.");
+
+        }
+    }
+}
